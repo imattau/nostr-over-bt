af8c8d05292ccdd6eb86d666787f9993a916abaa
imattau <imatt.au@protonmail.com>
2026-02-02 16:58:58 +1100

    Finalize NPM package metadata and exports

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// Core
+export { TransportManager } from './core/TransportManager.js';
+export { IdentityManager } from './core/IdentityManager.js';
+export { WoTManager } from './core/WoTManager.js';
+export { FeedManager } from './core/FeedManager.js';
+export { EventPackager } from './core/EventPackager.js';
+export { FeedIndex } from './core/FeedIndex.js';
+
+// Transports
+export { BitTorrentTransport } from './transport/BitTorrentTransport.js';
+export { NostrTransport } from './transport/NostrTransport.js';
+export { HybridTransport } from './transport/HybridTransport.js';
+
+// Interfaces
+export { ITransport } from './interfaces/ITransport.js';
